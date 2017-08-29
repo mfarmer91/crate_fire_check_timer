68dfa40cebe5582b721552fd189ece8c499ef00c
Michael Farmer <mfarmer91@gmail.com>
2017-08-29 13:49:12 -0400

    Added reminder component functionality based on elapsed state: shows @ 1800 seconds -- or 30 minutes

--- public/js/app.js
+++ public/js/app.js
@@ -13,10 +13,6 @@ class ReminderInterface extends React.Component {
                         title={'Fire-Check Reminder'}
                     />
                 </div>
-                <div className='reminder'>
-                    <Reminder 
-                    />
-                </div>
             </div>
         );
     }        
@@ -24,13 +20,12 @@ class ReminderInterface extends React.Component {
 
 class Timer extends React.Component {
     state = {
-        elapsed: 0,
+        elapsed: 1800,
     }
     
     tick = () => {
         this.setState({ elapsed: this.state.elapsed + 1});
     }
-
 
     onStartClick = () => {
         this.interval = setInterval(() => this.tick(), 1000);
@@ -41,38 +36,45 @@ class Timer extends React.Component {
     }
     
     render() {
+        let reminder = null;
+        if (this.state.elapsed >= 1800){
+            reminder = <Reminder />
+        }
+        
         return (
-        <div className='ui card'>
-            <div className='card'>
-                <div className='content'>
-                    <div id='timer_header'className='center aligned description'>
-                        <p>{this.props.title}</p>
+        <div>
+            <div className='ui card'>
+                <div className='card'>
+                    <div className='content'>
+                        <div id='timer_header'className='center aligned description'>
+                            <p>{this.props.title}</p>
+                        </div>
+                        <div id='time' className='center aligned description'>
+                            <h3>{toSeconds(this.state.elapsed)}</h3>
+                        </div>
                     </div>
-                    <div id='time' className='center aligned description'>
-                        <h3>{toSeconds(this.state.elapsed)}</h3>
-                    </div>
-                </div>
-                <div className='extra content'>
-                    <div className='ui two buttons'>
-                        <div className="ui basic green button" onClick={this.onStartClick}>Start</div>
-                        <div className="ui basic red button" onClick={this.onStopClick}>Stop</div>
+                    <div className='extra content'>
+                        <div className='ui two buttons'>
+                            <div className="ui basic green button" onClick={this.onStartClick}>Start</div>
+                            <div className="ui basic red button" onClick={this.onStopClick}>Stop</div>
+                        </div>
                     </div>
                 </div>
             </div>
+            {reminder}
         </div>
         );
     }        
 }
 
 class Reminder extends React.Component {
-    state = {
-        isShown: false,
-    }
     
     render() {
         return (
-            <div id='notification' className="ui warning compact message">
-                <p>Yo, fool: check the building to make sure it's not on fire!</p>
+            <div className='reminder'>
+                <div id='notification' className="ui warning compact message">
+                    <p>Yo, fool: check the building to make sure it's not on fire!</p>
+                </div>
             </div>
         );
     }        
@@ -83,4 +85,6 @@ ReactDOM.render(
   <ReminderInterface />,
   document.getElementById('content')
 );
+
+
 
